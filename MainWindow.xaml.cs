63e39fb1bf2c57f42495e28f82616d6a2134d030
Максим Котов <kotovmaks374@gmail.com>
2023-11-15 22:38:07 +0200

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Rectangle rectangle, rand_rectangle;
+            int x_1, x_2, y_1, y_2;
+            try
+            {
+                x_1 = Convert.ToInt32(x1.Text);
+                y_1 = Convert.ToInt32(y1.Text);
+                x_2 = Convert.ToInt32(x2.Text);
+                y_2 = Convert.ToInt32(y2.Text);
+                rectangle = new Rectangle(x_1, y_1, x_2, y_2);  
+                width.Content = rectangle.Width;
+                height.Content = rectangle.Height;
+
+                rand_rectangle = new Rectangle();
+                rand_x1.Content = rand_rectangle.X_1;
+                rand_y1.Content = rand_rectangle.Y_1;
+                rand_x2.Content = rand_rectangle.X_2;
+                rand_y2.Content = rand_rectangle.Y_2;
+                rand_width.Content = rand_rectangle.Width;
+                rand_height.Content = rand_rectangle.Height;
+            }
+            catch { MessageBox.Show("Помилка при введенні даних"); }
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_1
+{
+    internal class Rectangle
+    {
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+
+        public int X_1
+        {
+            get { return x1; }
+            set { x1 = value; }
+        }
+        public int X_2
+        {
+            get { return x2; }
+            set { x2 = value; }
+        }
+        public int Y_1
+        {
+            get { return y1; }
+            set { y1 = value; }
+        }
+        public int Y_2
+        {
+            get { return y2; }
+            set { y2 = value; }
+        }
+        public Rectangle()
+        {
+            x1 = new Random().Next(-5, 5);
+            y1 = new Random().Next(-5,5);
+            x2 = x1 + new Random().Next(1, 10);
+            y2 = y1 + new Random().Next(1, 10);
+        }
+        public Rectangle(int x1, int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        public int Width { get { return Math.Abs(x2 - x1); } }
+        public int Height { get { return Math.Abs(y2 - y1); } }
+        public void SetWidth(int width) { x2 = x1 + width; }
+        public void SetHeight(int height) { y2 = y1 + height; }
+    }
+
+}
